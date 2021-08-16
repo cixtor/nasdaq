f47c827244ff5d782b9dc1c42ddbdb78b352a5d3
Cixtor <cixtords@gmail.com>
2021-08-16 09:00:00 -0800

    Parse the date found in the last line of each file and return

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 mod accounts;
 
+use chrono::NaiveDate;
+use chrono::NaiveDateTime;
+use chrono::NaiveTime;
+use rev_lines::RevLines;
 use std::fs::File;
 use std::io::BufReader;
 
@@ -8,25 +12,28 @@ pub enum MyErrors {
     CannotOpenFile(std::io::Error),
     CannotReadFileInReverse(std::io::Error),
     FileHasNoLines,
+    LineWithoutCommas,
+    CannotParseDate(chrono::ParseError),
+    CannotCreateTime,
 }
 
 fn main() {
     for (tick, filename) in accounts::get_accounts().iter() {
         println!("{} {}", tick, filename);
 
-        if let Err(err) = read_last_date_from_file(filename) {
-            println!("{:?}", err);
-        }
+        let t = read_last_date_from_file(filename).unwrap();
+
+        println!("{}", t);
     }
 }
 
-fn read_last_date_from_file(filename: &str) -> Result<(), MyErrors> {
+fn read_last_date_from_file(filename: &str) -> Result<NaiveDateTime, MyErrors> {
     let file = match File::open(filename) {
         Ok(res) => res,
         Err(err) => return Err(MyErrors::CannotOpenFile(err)),
     };
 
-    let mut rev_lines = match rev_lines::RevLines::new(BufReader::new(file)) {
+    let mut rev_lines = match RevLines::new(BufReader::new(file)) {
         Ok(res) => res,
         Err(err) => return Err(MyErrors::CannotReadFileInReverse(err)),
     };
@@ -36,7 +43,22 @@ fn read_last_date_from_file(filename: &str) -> Result<(), MyErrors> {
         None => return Err(MyErrors::FileHasNoLines),
     };
 
-    println!("{}", last_line);
+    let date = match last_line.split(",").nth(0) {
+        Some(res) => res,
+        None => return Err(MyErrors::LineWithoutCommas),
+    };
 
-    Ok(())
+    let d = match NaiveDate::parse_from_str(date, "%Y-%m-%d") {
+        Ok(res) => res,
+        Err(err) => return Err(MyErrors::CannotParseDate(err)),
+    };
+
+    let t = match NaiveTime::from_hms_opt(9, 0, 0) {
+        Some(res) => res,
+        None => return Err(MyErrors::CannotCreateTime),
+    };
+
+    // TODO: Add one day to <date> to ignore already synced data.
+
+    Ok(d.and_time(t))
 }
